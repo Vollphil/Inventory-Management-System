e6e1e4800910b53f19e1e80873e1ae71e5578810
Philip Vollmann <Philip.vollmann0408@gmail.com>
2023-11-10 14:13:16 +0100

    initial commit

---
+++ Backend/Inventory-Management-System-Backend/src/test/java/com/example/InventoryManagementSystemBackend/InventoryManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InventoryManagementSystemBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
