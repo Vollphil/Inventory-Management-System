613ff0fcbd052a3ef8c17399cca8b7bd329a5171
Philip Vollmann <Philip.vollmann0408@gmail.com>
2023-11-11 14:46:57 +0100

    Created Simple Navbar with TailwindCss.

--- Frontend/Inventory-Management-System/src/Navbar/Navbar.jsx
+++ Frontend/Inventory-Management-System/src/Navbar/Navbar.jsx
@@ -1,12 +1,24 @@
 const Navbar = () => {
-    <div className="Navbar">
-        <nav>
-            <ul>
-                <li>
-                
-                </li>
-            </ul>
-        </nav>
-    </div>
+
+
+
+    return(
+    
+        <nav className="bg-indigo-700 shadow-lg">
+        <div className="container mx-auto">
+            <div className="flex justify-between items-center">
+                <a href="#" className="text-white text-3xl font-bold p-3">INVENTORY MANAGEMENT SYSTEM</a>
+                <ul className="text-gray-400 text-xl">
+                    <li className="inline-block">
+                        <a href="#" className="p-3 hover:text-white">Create Inventory</a>
+                    </li>
+                    {/* Add other list items here */}
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    
+    )
 }
 export default Navbar;
--- Frontend/Inventory-Management-System/src/main.jsx
+++ Frontend/Inventory-Management-System/src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css'
 import Layout from './Layout/Layout'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Layout/>
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
